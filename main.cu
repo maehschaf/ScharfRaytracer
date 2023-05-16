c8c635c7af49c5833387ed5c24f83eedfb959196
Sebastian Pohsner <sebastian.pohsner@kabelmail.de>
2023-05-17 01:17:32 +0200

    Tried using more blocks for samples -> way worse performance, shared memory access might be the reason for that

--- main.cu
+++ main.cu
@@ -9,60 +9,57 @@
 #include "stb_image_write.h"
 #include <iostream>
 
+__global__ void initFramebuffer(ViewRender* view) {
+	if (blockIdx.x == 0 && threadIdx.x == 0) {
+		for (size_t i = 0; i < view->pixelCount; i++) {
+			view->frameBuffer[i] = 0.f;
+		}
+	}
+}
+
 __global__ void renderScene(Scene* scene, ViewRender* view) {
 	//Get the pixel position
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	if ((x >= view->width) || (y >= view->height)) return;
+	int sample = blockIdx.z * blockDim.z + threadIdx.z;
+	if ((x >= view->width) || (y >= view->height) || sample >= view->samples) return;
 	int pixelIndex = (y * view->width + x) * CHANNELS;
 
 	//Create a random state
 	curandState randomState;
-	curand_init(23984, pixelIndex, 0, &randomState);
+	curand_init(23984, pixelIndex + sample, 0, &randomState);
 
-	float3 finalColor = make_float3(0, 0, 0);
-	for (int sample = 0; sample < view->samples; sample++) {
-
-		//What color should be the starting point?
-		float3 sampleColor = make_float3(1);
-		//Cast our ray
-		Ray ray = scene->camera.getRay((x + curand_uniform(&randomState) * 2 - 0.5f) / view->width, (y + curand_uniform(&randomState) * 2 - 0.5f) / view->height, static_cast<float>(view->height) / view->width);
-		Hit hit;
-		int bounce = 0;
-		for (; bounce < view->maxBounces; bounce++) {
-			hit = scene->raytrace(ray, hit.actor);
-			if (!hit.hit) {
-				//the world background
-				float sunStrength = 14;
-				float size = 0.05f;
-				float3 sunColor = make_float3(sunStrength);
-				float3 sunDir = normalize(make_float3(-1, .5f, .5));
-				float sharpness = 2.f;
-				float sunAngle = clamp((powf(dot(ray.getDirection(), sunDir), sharpness) - (1 - size)) * (1 / size), 0.f, 1.f);
-				sampleColor *= (1 - sunAngle) * scene->background.emissiveColor + sunAngle * sunColor;
-				break;
-			}
-			hit.actor->material.color(hit, ray, sampleColor, &randomState);
+	//What color should be the starting point?
+	float3 sampleColor = make_float3(1);
+	//Cast our ray
+	Ray ray = scene->camera.getRay((x + curand_uniform(&randomState) * 2 - 0.5f) / view->width, (y + curand_uniform(&randomState) * 2 - 0.5f) / view->height, static_cast<float>(view->height) / view->width);
+	Hit hit;
+	int bounce = 0;
+	for (; bounce < view->maxBounces; bounce++) {
+		hit = scene->raytrace(ray, hit.actor);
+		if (!hit.hit) {
+			//the world background
+			float sunStrength = 14;
+			float size = 0.05f;
+			float3 sunColor = make_float3(sunStrength);
+			float3 sunDir = normalize(make_float3(-1, .5f, .5));
+			float sharpness = 2.f;
+			float sunAngle = clamp((powf(dot(ray.getDirection(), sunDir), sharpness) - (1 - size)) * (1 / size), 0.f, 1.f);
+			sampleColor *= (1 - sunAngle) * scene->background.emissiveColor + sunAngle * sunColor;
+			break;
 		}
-		//Failed to hit a light source
-		if (bounce >= view->maxBounces) sampleColor *= 0;
-
-		finalColor += sampleColor;
+		hit.actor->material.color(hit, ray, sampleColor, &randomState);
 	}
-	finalColor /= view->samples;
+	//Failed to hit a light source
+	if (bounce >= view->maxBounces) sampleColor *= 0;
+
+	sampleColor /= view->samples;
 
 	//Set the final color
 	//finalColor = make_float3((float) x / view->width, (float) y / view->height, 0);
-	view->frameBuffer[pixelIndex + 0] = clamp(finalColor.x, 0.f, 1.f);
-	view->frameBuffer[pixelIndex + 1] = clamp(finalColor.y, 0.f, 1.f);
-	view->frameBuffer[pixelIndex + 2] = clamp(finalColor.z, 0.f, 1.f);
-}
-
-__global__ void initScene(Scene* scene, ViewRender* view) {
-	if (blockIdx.x == 0 && threadIdx.x == 0) {
-
-
-	}
+	view->frameBuffer[pixelIndex + 0] += sampleColor.x;
+	view->frameBuffer[pixelIndex + 1] += sampleColor.y;
+	view->frameBuffer[pixelIndex + 2] += sampleColor.z;
 }
 
 int main() {
@@ -74,7 +71,7 @@ int main() {
 	//Render settings!
 	new (d_view) ViewRender(1920, 1080, 16, 16); //be carefull with placement new....
 	d_view->maxBounces = 4;
-	d_view->samples = 400;
+	d_view->samples = 40;
 
 	Camera camera = Camera({ 0,0,0.3f }, { 1,1,0.2f });
 
@@ -88,37 +85,39 @@ int main() {
 	//Background Material
 	Material background(make_float3(0.6f, 0.8f, 1.0f) * 0.8f);
 	//Material background(make_float3(0));
-	
+
 	Scene* d_scene;
 	checkCudaErrors(cudaMallocManaged((void**)&d_scene, sizeof(Scene)));
 	//Scene setup
 	new (d_scene) Scene({ earth, a,b,c, mirror }, camera, background);
 
-	dim3 blocks(d_view->width / d_view->tileSizeX + 1, d_view->height / d_view->tileSizeY + 1);
-	dim3 threads(d_view->tileSizeX, d_view->tileSizeY);
-
 	std::cout << "Samples: " << d_view->samples << " Max Bounces: " << d_view->maxBounces << " Resolution: " << d_view->width << "x" << d_view->height << std::endl;
-	std::cout << "Setup time: " << (std::clock() - startTime) / (double) CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Scene Setup time: " << (std::clock() - startTime) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	startTime = std::clock();
+
+	dim3 blocks(d_view->width / d_view->tileSizeX + 1, d_view->height / d_view->tileSizeY + 1, d_view->samples);
+	dim3 threads(d_view->tileSizeX, d_view->tileSizeY, 1);
+
+	initFramebuffer << <1, 1 >> > (d_view);
+	checkCudaErrors(cudaGetLastError());
+	checkCudaErrors(cudaDeviceSynchronize());
 
 	renderScene << <blocks, threads >> > (d_scene, d_view);
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
 
-	std::cout << "Render time: " << (std::clock() - startTime) / (double) CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Render time: " << (std::clock() - startTime) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	startTime = std::clock();
 
 	//Convert float based rgb in frame buffer to only 8bit rgb for the out image
-	size_t outPixelsSize = d_view->width * d_view->height * CHANNELS;
-
-	unsigned char* outPixels = (unsigned char*)malloc(outPixelsSize);
-	for (size_t i = 0; i < outPixelsSize; i++) {
-		outPixels[i] = int(d_view->frameBuffer[i] * 255);
+	unsigned char* outPixels = (unsigned char*)malloc(d_view->pixelCount);
+	for (size_t i = 0; i < d_view->pixelCount; i++) {
+		outPixels[i] = int(clamp(d_view->frameBuffer[i], 0.f, 1.f) * 255);
 	}
 
 	stbi_write_jpg("out.jpg", d_view->width, d_view->height, CHANNELS, outPixels, 100);
 
-	std::cout << "Image output time: " << (std::clock() - startTime) / (double) CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Image output time: " << (std::clock() - startTime) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 
 	//Cleanup
 	free(outPixels);
